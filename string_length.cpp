35771589214071b89ac7ba92d6c651c5d43bac2d
chougulesumeru <chougulesumeru19@gmail.com>
2025-12-07 18:22:03 +0530

    files are ready to upload

---
+++ string_length.cpp
@@ -0,0 +1,22 @@
+// to find length of a string 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str="shiban darling";
+
+    cout << str.size() << endl;
+    cout << str.length() << endl;
+
+    cout << "using a lopp :- " ;
+
+    for(int i=0;i<=str[i];i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
