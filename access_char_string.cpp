de3045a0988fd80e19b6bfef9961176e8fcc6713
chougulesumeru <chougulesumeru19@gmail.com>
2025-12-07 18:39:28 +0530

    file ready

---
+++ access_char_string.cpp
@@ -0,0 +1,16 @@
+// to access a charatcer of a string
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "shiban darling";
+
+    cout << str[4] << endl;
+
+    cout << endl;
+
+    return 0;
+}
